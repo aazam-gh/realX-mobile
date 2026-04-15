0bec0874b539728e72c47e82a72b21941e3adffb
Othman Ayari <othmanayari049@gmail.com>
2026-04-15 15:58:18 +0300

    Add Arabic fallback label for percentage offers

--- app/vendor/[id].tsx
+++ app/vendor/[id].tsx
@@ -192,9 +192,14 @@ export default function VendorScreen() {
                     {/* Offers List */}
                     <View style={styles.offersList}>
                         {offers.map((offer) => {
-                            const offerTitle = isArabic
-                                ? (offer.titleAr || offer.titleEn)
-                                : (offer.titleEn || offer.titleAr);
+                            const percentValue =
+    offer.discountType === 'percentage' && offer.discountValue
+        ? ${offer.discountValue}%
+        : '';
+
+const offerTitle = isArabic
+    ? (offer.titleAr || (percentValue ? خصم  : offer.titleEn))
+    : (offer.titleEn || offer.titleAr);
 
                             return (
                                 <View key={offer.id} style={styles.offerCard}>
@@ -582,3 +587,4 @@ const styles = StyleSheet.create({
         color: '#666',
     },
 });
+
